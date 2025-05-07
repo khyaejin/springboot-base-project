3138aab5272b8f146a770d0a7c590b7ea2df6cd0
김혜진 <olivia019182@naver.com>
2025-05-08 04:33:58 +0900

    🔧 Update DemoApplication.java
    - @CreatedDate, @LastModifiedDate 사용을 위한 @EnableJpaAuditing추가
    - DB 연결 로그 추가

--- src/main/java/com/likelion/demo/DemoApplication.java
+++ src/main/java/com/likelion/demo/DemoApplication.java
@@ -1,8 +1,13 @@
 package com.likelion.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,4 +15,16 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner dbConnectionCheck(
+			@Value("${spring.datasource.url}") String dbUrl,
+			@Value("${spring.datasource.username}") String dbUsername
+	) {
+		return args -> {
+			System.out.println("=== Database Connection Info ===");
+			System.out.println("URL: " + dbUrl);
+			System.out.println("Username: " + dbUsername);
+			System.out.println("================================");
+		};
+	}
 }
