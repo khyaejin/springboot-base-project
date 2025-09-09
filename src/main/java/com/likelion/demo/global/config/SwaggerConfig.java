ca792e6064f45a2ef004cfaadd15c7a035400fa7
김혜진 <olivia019182@naver.com>
2025-09-09 10:19:49 +0900

    fix: SwaggerConfig 속 그룹 ID 수정

--- src/main/java/com/likelion/demo/global/config/SwaggerConfig.java
+++ src/main/java/com/likelion/demo/global/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.likelion.demo"))
+                .apis(RequestHandlerSelectors.basePackage("com.base.demo"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
